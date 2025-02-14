55fc3e07348c75f0cac43847bba40249b923f2da
SavioPetrus <savio.petrus.vieira@gmail.com>
2025-02-14 09:13:02 -0400

    Chore: Implementação da funcionalidade de aplicar o efeito riscado aos itens da lista, houve também algumas mudanças na estilização do input e do button cadastrar.

--- main.js
+++ main.js
@@ -1,51 +1,31 @@
-const form = document.getElementById('addTask');
-const task = [];
 const errorMsg = document.querySelector('.errorMsg');
 const sucessMsg = document.querySelector('.sucessMsg');
+const task = [];
 
-let lines = '';
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    errorMsg.style.display = 'none';
-    sucessMsg.style.display = 'none';
-    newTask();
-    refreshList();
-});
-
-function newTask(){
-    const inputTask = document.getElementById('taskName');
-    inputTask.value = inputTask.value.toUpperCase();
-    if(inputTask.value === ''){
-        errorMsg.innerText = 'Por favor preencha o nome da tarefa';
-        errorMsg.style.display = 'block';
-        return
-    }else if(task.includes(inputTask.value)){
-        errorMsg.innerText = `A tarefa: ${inputTask.value} já está na lista`;
-        errorMsg.style.display = 'block';
-    }else{
-        sucessMsg.innerText = 'Nova tarefa registrada!';
-        sucessMsg.style.display = 'block';
-        task.push(inputTask.value);
-
-        let line = '<ul>'
-        line += `<li>${inputTask.value}</li>`
-        line += `</ul>`
-
-        lines += line;
-    }
-    
-    
-
-    inputTask.addEventListener('input', function(){
-        sucessMsg.style.display = 'none';
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
         errorMsg.style.display = 'none';
+        sucessMsg.style.display = 'none';
+        const novaTarefa = $('#taskName').val().toUpperCase();
+        const novoItem = $('<li></li>');
+        if(novaTarefa === ''){
+            errorMsg.innerText = 'Por favor preencha o nome da tarefa';
+            errorMsg.style.display = 'block';
+        }else if(task.includes(novaTarefa)){
+            errorMsg.innerText = `A tarefa: ${novaTarefa} já está na lista`;
+            errorMsg.style.display = 'block';
+        }else{
+            sucessMsg.innerText = 'Nova Tarefa registrada!';
+            sucessMsg.style.display = 'block';
+            task.push(novaTarefa);
+            $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
+            $(novoItem).appendTo(`ul`);
+        }
+        
+    })
+
+    $(document).on('click', 'li', function(){
+        $(this).toggleClass('tachado');
     });
-
-    inputTask.value = '';
-}
-
-function refreshList(){
-    const listBody = document.querySelector('ul');
-    listBody.innerHTML = lines;
-}
+})
